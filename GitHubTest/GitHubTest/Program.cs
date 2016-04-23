de401d6da4dbce853257e0648e74bdc66dd77df1
Dominic Brennan <s00002396@mail.itsligo.ie>
2016-04-23 21:50:23 +0100

    changed day to Sunday

--- GitHubTest/GitHubTest/Program.cs
+++ GitHubTest/GitHubTest/Program.cs
@@ -10,7 +10,7 @@ namespace GitHubTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Today is Saturday");
+            Console.WriteLine("Tomorrow is Sunday");
         }
     }
 }
